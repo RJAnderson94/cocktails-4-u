8c9aa3c3464985151b3eaac725f7dbc7775b1648
Ryan Anderson <RJAnderson94@gmail.com>
2020-10-15 11:41:19 +0000

    search bar created, add a cocktial page and cocktail home page created and linked

--- app.py
+++ app.py
@@ -5,8 +5,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def base():
-    return render_template('base.html')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/add_cocktail')
+def add_cocktail():
+    return render_template('addcocktail.html')
+
+
+@app.route('/cocktail_page')
+def cocktail_page():
+    return render_template('cocktailpage.html')
 
 
 if __name__ == '__main__':
